825e0209f4a47096503808442cf0a676f402ee2c
John Ratana <jratana1@gmail.com>
2021-01-30 15:49:20 -0500

    commit tree 426f543a595e52d3cd9a75a26cb14abfff77e2ca

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const testVar = {}
 
 function testFunc() {
+  
   return "hi"
+}
+
+function superbowlWin(array) {
+    let find = array.find(function(object) {return object.result === "W"})
+
+    if (find) {
+      return find.year
+    }
+    else {
+      undefined
+    }
 }
